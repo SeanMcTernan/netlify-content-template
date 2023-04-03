2ffce666a47eaa530c7a17fdebe51aa97f6023b1
Sean Mc Ternan <seanmcternan@gmail.com>
2023-04-03 16:32:23 -0600

    remove sub message from experience preview

--- components/experience-preview.js
+++ components/experience-preview.js
@@ -13,11 +13,6 @@ export default function ExperiencePreview({
       <div className="mb-5">
         {company && <Company name={company.name} picture={company.logo} link={`experience/${slug}`} />}
       </div>
-      <h3 className="text-3xl mb-3 leading-snug">
-        <Link href={`experience/${slug}`} className="hover:underline">
-          {title}
-        </Link>
-      </h3>
       <p className="text-lg leading-relaxed mb-4">{excerpt} <a href={`experience/${slug}`} className="underline hover:text-success duration-200 transition-colors">Read More.</a></p>
     </div>
   )
