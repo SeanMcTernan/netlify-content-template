cc851172c10338c8990bb20e6950a42a8af28e88
Sean Mc Ternan <seanmcternan@gmail.com>
2023-04-03 16:31:14 -0600

    remove dates from experience

--- components/experience-preview.js
+++ components/experience-preview.js
@@ -4,8 +4,6 @@ import DateComponent from './date'
 
 export default function ExperiencePreview({
   title,
-  startDate,
-  endDate,
   excerpt,
   company,
   slug,
@@ -20,9 +18,6 @@ export default function ExperiencePreview({
           {title}
         </Link>
       </h3>
-      <div className="text-lg mb-4">
-        <DateComponent startDateString={startDate} endDateString={endDate} />
-      </div>
       <p className="text-lg leading-relaxed mb-4">{excerpt} <a href={`experience/${slug}`} className="underline hover:text-success duration-200 transition-colors">Read More.</a></p>
     </div>
   )
--- components/footer.js
+++ components/footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h4 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            © 2023. All rights reserved
+            © 2023 - All rights reserved
           </h4>
         </div>
       </Container>
