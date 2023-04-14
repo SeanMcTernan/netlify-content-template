aac3a7bca3a1c74bbcbe313209b812b2a86b5245
Sean Mc Ternan <seanmcternan@gmail.com>
2023-04-13 18:46:11 -0600

    Fix broken template

--- components/intro.js
+++ components/intro.js
@@ -37,7 +37,7 @@ export default function Intro({ profile }) {
         {profile.personal.unitName}
       </h1>
       <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-        <p>{profile.bio}</p>
+        <p>{profile.unitDescription}</p>
       </section>
     </>
   )
